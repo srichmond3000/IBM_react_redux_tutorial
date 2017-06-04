a9f8ee31bccfa4d29d7287e5b54253c726d14dc8
srichmond3000 <srichmond3000@googlemail.compwd>
2017-06-04 12:03:12 +0100

    Initial setup - gets something showing in the browser.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Hello from IBM tutorial!</h1>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+'use strict'
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true,
+    inline: true,
+    port: 4000
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      },
+      {
+        test:/\.js?$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      }
+    ]
+  }
+};
